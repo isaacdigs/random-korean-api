f75413b4940a08a2f65145a50f374876d5e639f8
isaacdigs <pk07057@gmail.com>
2022-05-11 22:21:40 +0900

    Make basic task manager boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# random-korean-api
-Aimed to build an API that returns random data for Korean content such as long text, user data, and posts.
+# random-korean
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <ButtonComponent text="Add Task" color = "green"/>
+    </header>
+</template>
+
+<script>
+import ButtonComponent from './Button'
+
+export default {
+    name: 'HeaderComponent',
+    props: {
+        title: String,
+    },
+    components: {
+        ButtonComponent
+    }
+}
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
